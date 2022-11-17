8d227559d28b6516fd8a866ba35ee1841a89de35
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2022-11-17 21:44:44 +0300

    refactor: comments

--- script.js
+++ script.js
@@ -5,13 +5,23 @@
 const player0El = document.querySelector('player--0');
 const player1El = document.querySelector('player--1');
 
+//// main scores
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+
+//// current scores
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
+//// dice
 const diceEl = document.querySelector('.dice');
+
+//// buttons
+////// new game
 const btnNew = document.querySelector('.btn--new');
+////// roll dice
 const btnRoll = document.querySelector('.btn--roll');
+////// hold
 const btnHold = document.querySelector('.btn--hold');
 
 // starting conditions
